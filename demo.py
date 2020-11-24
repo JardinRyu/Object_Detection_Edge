714ee4deced275a65517e625b956b4c6db12ee89
Jeong Won Ryu <54166852+JardinRyu@users.noreply.github.com>
2020-11-24 17:32:38 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,17 +3,27 @@ import numpy as np
 import cv2
 import jetson.inference
 import jetson.utils
+import datetime
  
 # setup the network we are using
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture('video.mp4')
-cap.set(3,640)
-cap.set(4,480)
- 
+
+CAMERA_WIDTH = 1280
+CAMERA_HEIGHT = 720
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+record_org = False
+record = False
+
+capture = cv2.VideoCapture(0)
+if (capture.isOpened() == False):
+    print("NO CAMERA!")
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
+capture.set(cv2.CAP_PROP_FPS, 30)
+
 while (True):
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (640, 480))
+    ret, frame = capture.read()
+    
     w = frame.shape[1]
     h = frame.shape[0]
     # to RGBA
@@ -44,9 +54,41 @@ while (True):
     cv2.putText(result, "Total: " + str(count), (10, 45), font, 0.5, (240, 240, 240), 1, line)
     # show frames
     cv2.imshow('frame', result)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
+    k = cv2.waitKey(1) & 0xff
+    
+    if k == ord('q') or k == 27:
+            break
+    #Capture (Original)
+    elif k == ord('c'):
+        print('Capture (Original)')
+        cv2.imwrite("./" + now + ".png", frame)
+    #Capture (Detection)
+    elif k == ord('C'):
+        print('Capture (Detection)')
+        cv2.imwrite("./" + now + ".png", result)
+    #Start Record (Original)
+    elif k == ord('r'):
+        print("Start Record (Original)")
+        record_org = True
+        video = cv2.VideoWriter("./" + now + ".avi", fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+    #Start Record (Detection)
+    elif k == ord('R'):
+        print("Start Record (Detection)")
+        record = True
+        video = cv2.VideoWriter("./" + now + ".avi", fourcc, 20.0, (result.shape[1], result.shape[0]))
+    #Stop Record
+    elif k == ord('s'):
+        print("Stop Record")
+        record_org = False
+        record = False
+        
+    if record_org == True:            
+        video.write(frame)
+    if record == True:            
+        video.write(result)
  
 # When everything done, release the capture
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
