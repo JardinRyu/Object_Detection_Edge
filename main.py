f84b2e0fbf0af7a60df91e2e9161225a1967d5ee
Jeong Won Ryu <54166852+JardinRyu@users.noreply.github.com>
2020-11-24 12:56:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ import coco
 import uff
 import tensorrt as trt
 import graphsurgeon as gs
-#from config import model_ssd_inception_v2_coco_2017_11_17 as model
-#from config import model_ssd_mobilenet_v1_coco_2018_01_28 as model
+
 from config import model_ssd_mobilenet_v2_coco_2018_03_29 as model
 
 ctypes.CDLL("lib/libflattenconcat.so")
