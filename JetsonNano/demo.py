ea828fe2fee792d07ff525e06a28f22ff6fbf100
Jeong Won Ryu <54166852+JardinRyu@users.noreply.github.com>
2020-12-03 14:49:35 +0900

    Update demo.py

--- JetsonNano/demo.py
+++ JetsonNano/demo.py
@@ -4,58 +4,70 @@ import jetson.inference
 import jetson.utils
 import datetime
  
-# setup the network we are using
+# Detection 모델 setup
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-
+#camera 해상도 설정 값
 CAMERA_WIDTH = 1280
 CAMERA_HEIGHT = 720
+#화면녹화 설정
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 record_org = False
 record = False
-
+#camera 입력 /dev/video0
 capture = cv2.VideoCapture(0)
 if (capture.isOpened() == False):
     print("NO CAMERA!")
+#camera 해상도 설정
 capture.set(3, CAMERA_WIDTH)
 capture.set(4, CAMERA_HEIGHT)
 
 while (True):
+    #frame 받기, ret : camera 상태값
     ret, frame = capture.read()
     
     w = frame.shape[1]
     h = frame.shape[0]
+    
+    #이미지를 모델 input값으로 변환
     # to RGBA
     # to float 32
     input_image = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA).astype(np.float32)
     # move the image to CUDA:
     input_image = jetson.utils.cudaFromNumpy(input_image)
- 
+    # 모델 추론
     detections = net.Detect(input_image, w, h)
+    # 감지 객체 count
     count = len(detections)
-    
+    # 감지 객체 print
     for detection in detections:
         print(detection)
  
     # print out timing info
     net.PrintProfilerTimes()
+    
+    #이미지를 출력 포맷으로 변경
     # Display the resulting frame
     numpyImg = jetson.utils.cudaToNumpy(input_image, w, h, 4)
     # now back to unit8
     result = numpyImg.astype(np.uint8)
-    # Display fps
+    
+    # fps 계산
     fps = 1000.0 / net.GetNetworkTime()
+    # fps, count 값 출력
     font = cv2.FONT_HERSHEY_SIMPLEX
     line = cv2.LINE_AA
     cv2.putText(result, "FPS: " + str(int(fps)) + ' | Detecting', (11, 20), font, 0.5, (32, 32, 32), 4, line)
     cv2.putText(result, "FPS: " + str(int(fps)) + ' | Detecting', (10, 20), font, 0.5, (240, 240, 240), 1, line)
     cv2.putText(result, "Total: " + str(count), (11, 45), font, 0.5, (32, 32, 32), 4, line)
     cv2.putText(result, "Total: " + str(count), (10, 45), font, 0.5, (240, 240, 240), 1, line)
-    # show frames
-    cv2.imshow('frame', result)
-
-    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
-    k = cv2.waitKey(1) & 0xff
     
+    # 영상 출력
+    cv2.imshow('frame', result)
+    # 저장파일명 설정 (날짜, 시간)
+    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
+    #키 입력
+    k = cv2.waitKey(1) & 0xff
+    #종료
     if k == ord('q') or k == 27:
             break
     #Capture (Original)
